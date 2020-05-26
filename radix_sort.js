f38bfae98417bb46e5c65e8b45863a57e1c08791
bcwan <bcwan8@gmail.com>
2020-05-26 10:40:10 -0700

    Complete radix_sort.js notes

--- radix_sort.js
+++ radix_sort.js
@@ -4,3 +4,37 @@
 // floor to round away decimals
 // mod 10 to get the digit of interest
 const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+//getIntLength
+// find number of digits of the largest number in the list
+// know how many times to run through algorithmic process
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+
+//getMaxDigits
+function getMaxDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+  }
+  return maxDigits;
+}
+
+//for positive integers
+function radixSort(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+
+  let maxDigits = getMaxDigits(arr);
+  for (let k = 0; k < maxDigits; k++) {
+    let buckets = Array.from({length: 10}, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      let digit = getDigitFrom(arr[i], k);
+      buckets[digit].push(arr[i]);
+    }
+    arr = [].concat(...buckets);
+  }
+  return arr;
+}
+
+//Radix Sort will have run time of O(n * k), with k being the longest length digit
