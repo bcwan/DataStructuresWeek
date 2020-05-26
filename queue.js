6afec3dafcb8f318abebf622ed1a1dee5999c3d7
bcwan <bcwan8@gmail.com>
2020-05-26 15:41:07 -0700

    Add enqueue in queue.js

---
+++ queue.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if(!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    return ++this.length;
+  }
+  
+}
