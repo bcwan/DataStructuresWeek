db1611588b158c76c289fee5bdb7af5d68a47942
bcwan <bcwan8@gmail.com>
2020-05-26 15:12:15 -0700

    Add pop and size method in stack.js

--- stack.js
+++ stack.js
@@ -12,4 +12,35 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    const temp = this.top;
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
