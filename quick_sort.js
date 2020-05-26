7720619a7bd23751b2773c30bc3991e80a9896d5
bcwan <bcwan8@gmail.com>
2020-05-26 09:59:57 -0700

    Push quicksort

---
+++ quick_sort.js
@@ -0,0 +1,16 @@
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array.shift();
+  let left = array.filter(el => el < pivot);
+  let right = array.filter(el => el >= pivot);
+
+  let leftSorted = quickSort(left);
+  let rightSorted = quickSort(right);
+
+  return [...leftSorted, pivot, ...rightSorted];
+
+}
+
