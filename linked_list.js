cedce591025a8ac2f3a102a554364542f25fb7e4
bcwan <bcwan8@gmail.com>
2020-05-26 15:05:23 -0700

    Construct stack ADT

---
+++ linked_list.js
@@ -0,0 +1,7 @@
+//Time complexity
+  //access: O(n)
+  //search: O(n)
+  //insertion: O(1)
+  //deletion: O(1)
+
+  
---
+++ stack.js
@@ -0,0 +1,15 @@
+// stack ADT with Linked List
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+}
