5b9f35aa781baf1574aa596ece0a04c600173d12
bcwan <bcwan8@gmail.com>
2020-05-27 15:57:14 -0700

    Add remove and additional linked list methods

--- linked_list.js
+++ linked_list.js
@@ -91,27 +91,56 @@ class LinkedList {
 
   // TODO: Implement the get method here
   get(index) {
-
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
   }
 
   // TODO: Implement the set method here
   set(index, val) {
-
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.value = val;
+      return true;
+    }
+    return false;
   }
 
   // TODO: Implement the insert method here
   insert(index, val) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === this.length) return !!this.addToTail(val);
+    if (index === 0) return !!this.addToHead(val);
 
+    const newNode = new Node(val);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
   }
 
   // TODO: Implement the remove method here
   remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.removeHead();
+    if (index === this.length - 1) return this.removeTail();
+    const previousNode = this.get(index - 1);
+    const removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
 
   }
 
   // TODO: Implement the size method here
   size() {
-
+    return this.length;
   }
 }
-
